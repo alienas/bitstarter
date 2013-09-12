39af8ccef22e3c521c509df950504d0289123a11
cm <no.clue.at.all@gmail.com>
2013-09-12 05:42:43 +0000

    updated web.js 2

--- web.js
+++ web.js
@@ -2,8 +2,18 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var outfile = 'index.html';
+var data = new Buffer(50);
+
+data= fs.readFileSync(outfile);
+//len = data.write('\u00bd + \u00bc = \u00be', 0);
+//len= data.length();
+
+console.log (data.toString('utf8'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(data.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
